cd6fb97bdd88b4a7b48f3a0a42feba5db3d1e8a4
IShamss <mohamedshams2000@gmail.com>
2020-03-21 13:25:14 +0200

    Updates

---
+++ precache-manifest.a64673f95896ac97f39490feb917c8e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff0c52f1354c6ca39dba1e71e7c4e5a",
+    "url": "/Art-Gallery/index.html"
+  },
+  {
+    "revision": "8e955f21ffaa34318860",
+    "url": "/Art-Gallery/static/css/main.1d9c2b56.chunk.css"
+  },
+  {
+    "revision": "b3bb6f7ea74c5e54d43f",
+    "url": "/Art-Gallery/static/js/2.04a5f1e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Art-Gallery/static/js/2.04a5f1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e955f21ffaa34318860",
+    "url": "/Art-Gallery/static/js/main.12d577c3.chunk.js"
+  },
+  {
+    "revision": "f0374362309d02c624d1",
+    "url": "/Art-Gallery/static/js/runtime-main.f7cad491.js"
+  }
+]);
